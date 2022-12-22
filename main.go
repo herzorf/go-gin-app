4f0ba526c25d1c48951e84dcad98076599d5ed73
何中峰 <1446450047@qq.com>
2022-12-22 23:43:14 +0800

    连接数据库

--- main.go
+++ main.go
@@ -1,12 +1,22 @@
 package main
 
 import (
+	"github.com/gin-gonic/gin"
 	"github.com/spf13/viper"
+	"githun.com/herzorf/go-gin-app/common"
 	"os"
 )
 
 func main() {
 	initConfig()
+	common.InitDB()
+
+	route := gin.Default()
+	port := viper.GetString("server.port")
+	err := route.Run(":" + port)
+	if err != nil {
+		panic(err)
+	}
 }
 
 func initConfig() {
